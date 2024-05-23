a7a7dd0e13dd8d3b081d0ff7eb238e423fa7773d
Craig Belpedio <craigular.b@gmail.com>
2024-05-23 17:13:10 -0500

    messing with logging

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import logging
 from pathlib import Path
 from os import listdir
 from flask import Flask, jsonify, render_template, request
 from scraper import scrape
 
 app = Flask(__name__)
+app.logger.level = logging.INFO
 _IMAGE_EXTENSIONS = {'.jpg', '.jpeg', '.png'}
 _SECONDS_TO_MILLISECONDS = 1000
 
@@ -12,10 +14,12 @@ def _is_image_file(filename):
     return Path(filename).suffix.lower() in _IMAGE_EXTENSIONS
 
 
-def _get_intervals():
-    rotate = int(request.args.get('rotate', 10)) * _SECONDS_TO_MILLISECONDS
-    refresh = int(request.args.get('refresh', 3600)) * _SECONDS_TO_MILLISECONDS
-    return rotate, refresh
+def _get_intervals_with_scale():
+    rotate = int(request.args.get('rotate', 10))
+    refresh = int(request.args.get('refresh', 3600))
+    app.logger.info(f'Rotate interval set to {rotate} seconds')
+    app.logger.info(f'Refresh interval set to {refresh} seconds')
+    return rotate * _SECONDS_TO_MILLISECONDS, refresh * _SECONDS_TO_MILLISECONDS
 
 
 @app.route('/')
@@ -25,7 +29,7 @@ def root():
 
 @app.route('/bookshelf')
 def bookshelf():
-    rotate, refresh = _get_intervals()
+    rotate, refresh = _get_intervals_with_scale()
     image_dir = Path(app.static_folder) / 'images'
     images = sorted([f'images/{filename}' for filename in listdir(image_dir) if _is_image_file(filename)])
     return render_template('bookshelf.html', images=images, rotate=rotate, refresh=refresh)
@@ -47,4 +51,4 @@ def update():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
